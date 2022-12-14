5d17ab06896d0f69d1db4d223dec7a410aa8ca01
ANDERSON SILVA <alucardand2017@gmail.com>
2022-12-14 00:26:58 -0300

    colocação de exceções e laços try/catch para prevenção de quebras no programa.

--- QRCodeACMnfp/Form1.cs
+++ QRCodeACMnfp/Form1.cs
@@ -35,24 +35,32 @@ namespace QRCodeACMnfp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                SetupDataGridView();
+                rdbManual.Checked = true;
+                driver.Navigate().GoToUrl("https://www.nfp.fazenda.sp.gov.br/EntidadesFilantropicas/ListagemNotaEntidade.aspx");
+                LimpaEFocaQRCode();
+            }
+            catch (Exception)
+            {
 
-            SetupDataGridView();
-            rdbManual.Checked = true;
-            driver.Navigate().GoToUrl("https://www.nfp.fazenda.sp.gov.br/EntidadesFilantropicas/ListagemNotaEntidade.aspx");
-            LimpaEFocaQRCode();
+                throw new WebDriverArgumentException("A versão do Navegador Chrome não está atualizada corretamente");
+            }
+
         }
 
 
         //FUNÇÕES
         private void txtQRCode_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
-            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            try
             {
-                QuebrarCodigoQR(txtQRCode.Text);
-                bool testeForms = TestaInformacoes();
-                if (testeForms && Notas.Count < capacidadePlanilha)
+                if (e.KeyCode == System.Windows.Forms.Keys.Enter)
                 {
-                    try
+                    QuebrarCodigoQR(txtQRCode.Text);
+                    bool testeForms = TestaInformacoes();
+                    if (testeForms && Notas.Count < capacidadePlanilha)
                     {
                         SeleniumSetMethods.EnterText(driver, txtCNPJ.Text, txtData.Text, mskValor.Text.Replace(".", "").Replace(",", ""), txtExtrato.Text);
                         SeleniumSetMethods.SelectDropDown(driver, "ddlTpNota", "Cupom Fiscal", "Id");
@@ -62,30 +70,61 @@ namespace QRCodeACMnfp
                         MostrarNoGridView();
                         lancamentos++;
                         AtualizarValoresTotaisNaTela();
+                        txtQRCode.Text.Trim();
+                        LimpaEFocaQRCode();
                     }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Erro na hora de enviar as informações a página, verifique se está logad@" +
-                            " no ambiente correto (página aberta pelo próprio programa quando iniciado. Código = ", ex.Message);
-                    }
-                    txtQRCode.Text.Trim();
-                    LimpaEFocaQRCode();
                 }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro na visualização e estrutura da planilha");
+                LimpaEFocaQRCode();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro de  aplicação.");
+                LimpaEFocaQRCode();
+
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro de argumento inválido!");
+                LimpaEFocaQRCode();
+
+            }
+            catch (WebDriverArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Erro na captura do Botão.");
+                LimpaEFocaQRCode();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Verifique se está logado e na página de inserção de notas fiscais corretamente.", "Erro Geral no envio de dados para tabela", MessageBoxButtons.OK);
+                LimpaEFocaQRCode();
+
             }
         }
 
         private void AtualizarValoresTotaisNaTela()
         {
-            lblNLancamentos.Text = lancamentos.ToString();
-            float soma = 0;
-            foreach (NotaFiscalValoresl nota in Notas)
-                soma +=  float.Parse(nota.Valor);
-            lblValorTotal.Text = soma.ToString("F");
+            try
+            {
+                lblNLancamentos.Text = lancamentos.ToString();
+                float soma = 0;
+                foreach (NotaFiscalValoresl nota in Notas)
+                    soma += float.Parse(nota.Valor);
+                lblValorTotal.Text = soma.ToString("F");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Houve problemas com a leitura dos dados. Erro na conversão de valores Totais no App.");
+            }
+
         }
 
         private void LimpaEFocaQRCode()
         {
-            txtQRCode.Focus();
+            FocoPrincipal();
             txtQRCode.Text = string.Empty;
             txtQRCode.ClearUndo();
             txtCNPJ.Clear();
@@ -94,22 +133,24 @@ namespace QRCodeACMnfp
             mskValor.Clear();
         }
 
-        private bool TestaInformacoes()
+        private bool TestaInformacoes() //exception ok
         {
             var testaValor = float.TryParse(mskValor.Text.Replace(".", ","), out float valor);
-            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            if (!testaValor)
+            {
+                throw new ArgumentException("Não foi possível transformar o valor em um número válido");
+            }
             if (txtCNPJ.Text.Length != 14)
             {
-                MessageBox.Show("CNPJ da nota não possui 14 dígitos");
-                return false;
+                throw new ApplicationException("CNPJ da nota não possui 14 dígitos");
             }
             if (txtData.Text.Length != 8)
             {
-                MessageBox.Show("A data deve estár no formato ddMMYYYY sem espaço ou caracteres especiais.");
-                return false;
+                throw new ApplicationException("A data deve estár no formato ddMMYYYY sem espaço ou caracteres especiais.");
             }
-            if (testaValor && valor > 2000)
+            if (valor > 2000)
             {
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
                 var result = MessageBox.Show("Deseja confirmar?", "VALOR ANORMAL!", buttons);
                 if (result == System.Windows.Forms.DialogResult.No)
                 {
@@ -119,14 +160,13 @@ namespace QRCodeACMnfp
             return true;
         }
 
-        private void QuebrarCodigoQR(string qRCodeAll)
+        private void QuebrarCodigoQR(string qRCodeAll) //exception ok
         {
             if (qRCodeAll.Length > 60)
             {
                 var qrCodeLimpo = qRCodeAll.Trim();
                 var cnpj = qrCodeLimpo.Substring(6, 14);
                 var extrato = qrCodeLimpo.Substring(31, 6);
-
                 var dataAno = qrCodeLimpo.Substring(45, 4);
                 var dataMes = qrCodeLimpo.Substring(49, 2);
                 var dataDia = qrCodeLimpo.Substring(51, 2);
@@ -140,8 +180,9 @@ namespace QRCodeACMnfp
             }
             else
             {
-                MessageBox.Show("CQ code não foi lido corretamente ou é incompatível com essa aplicação.");
                 LimpaEFocaQRCode();
+                throw new ArgumentException("Erro ao escanear elementos do QRCode. Verifique a compatibilidade" +
+                    "do código ou se escaneou sem querer o código de barras próximo.");
             }
         }
 
@@ -194,7 +235,7 @@ namespace QRCodeACMnfp
             this.dataGridView1.Columns[9].DefaultCellStyle.Format = "N2";
         }
 
-        private void MostrarNoGridView()
+        private void MostrarNoGridView() //exception ok
         {
             int linha = 0;
             int coluna = 0;
@@ -206,8 +247,8 @@ namespace QRCodeACMnfp
                     coluna++;
                     if (coluna > 10)
                     {
-                        MessageBox.Show("Não é possível adicionar mais valores!");
-                        break;
+                        throw new ArgumentOutOfRangeException("Não é mais possível lançar novos registros, " +
+                            "Limite de campos foi alcançado.");
                     }
                     else
                     {
@@ -227,50 +268,57 @@ namespace QRCodeACMnfp
 
         private void GerarRelatorio()
         {
-            DataTable dt = new DataTable();
-            for (int i = 1; i <= 11; i++)
+            try
             {
-                dt.Columns.Add("Coluna " + i, typeof(float));
-            }
-            string dataRelatorio = DateTime.Now.ToString("ddMMyyyyHHmmss");
-            using (var workbook = new XLWorkbook())
-            {
-
-                foreach (DataGridViewColumn coluna in dataGridView1.Columns)
+                DataTable dt = new DataTable();
+                for (int i = 1; i <= 11; i++)
                 {
-                    dt.Columns.Add(coluna.Name, typeof(float));
+                    dt.Columns.Add("Coluna " + i, typeof(float));
                 }
-                foreach (DataGridViewRow linha in dataGridView1.Rows)
+                string dataRelatorio = DateTime.Now.ToString("ddMMyyyyHHmmss");
+                using (var workbook = new XLWorkbook())
                 {
-                    DataRow drow = dt.NewRow();
-                    foreach (DataGridViewCell cell in linha.Cells)
+
+                    foreach (DataGridViewColumn coluna in dataGridView1.Columns)
                     {
-                        if (cell.Value == null)
-                        {
-                            cell.Value = 0f;
-                        }
-                        drow[cell.ColumnIndex] = cell.Value.ToString();
+                        dt.Columns.Add(coluna.Name, typeof(float));
                     }
-                    dt.Rows.Add(drow);
+                    foreach (DataGridViewRow linha in dataGridView1.Rows)
+                    {
+                        DataRow drow = dt.NewRow();
+                        foreach (DataGridViewCell cell in linha.Cells)
+                        {
+                            if (cell.Value == null)
+                            {
+                                cell.Value = 0f;
+                            }
+                            drow[cell.ColumnIndex] = cell.Value.ToString();
+                        }
+                        dt.Rows.Add(drow);
+                    }
+
+                    var worksheet = workbook.AddWorksheet("Plan1");
+                    worksheet.Cell("A1").InsertData(dt);
+                    worksheet.Cell("A51").FormulaA1 = "=SUM(A1:A50)";
+                    worksheet.Cell("B51").FormulaA1 = "=SUM(B1:B50)";
+                    worksheet.Cell("C51").FormulaA1 = "=SUM(C1:C50)";
+                    worksheet.Cell("D51").FormulaA1 = "=SUM(D1:D50)";
+                    worksheet.Cell("E51").FormulaA1 = "=SUM(E1:E50)";
+                    worksheet.Cell("F51").FormulaA1 = "=SUM(F1:F50)";
+                    worksheet.Cell("G51").FormulaA1 = "=SUM(G1:G50)";
+                    worksheet.Cell("H51").FormulaA1 = "=SUM(H1:H50)";
+                    worksheet.Cell("I51").FormulaA1 = "=SUM(I1:I50)";
+                    worksheet.Cell("J51").FormulaA1 = "=SUM(J1:J50)";
+                    worksheet.Cell("K51").FormulaA1 = "=SUM(A51:J51)";
+
+
+                    workbook.SaveAs(enderecoSave + "Relatorio" + dataRelatorio + ".xlsx");
+                    MessageBox.Show("Relatório Gerado em " + enderecoSave + " .");
                 }
-
-                var worksheet = workbook.AddWorksheet("Plan1");
-                worksheet.Cell("A1").InsertData(dt);
-                worksheet.Cell("A51").FormulaA1 = "=SUM(A1:A50)";
-                worksheet.Cell("B51").FormulaA1 = "=SUM(B1:B50)";
-                worksheet.Cell("C51").FormulaA1 = "=SUM(C1:C50)";
-                worksheet.Cell("D51").FormulaA1 = "=SUM(D1:D50)";
-                worksheet.Cell("E51").FormulaA1 = "=SUM(E1:E50)";
-                worksheet.Cell("F51").FormulaA1 = "=SUM(F1:F50)";
-                worksheet.Cell("G51").FormulaA1 = "=SUM(G1:G50)";
-                worksheet.Cell("H51").FormulaA1 = "=SUM(H1:H50)";
-                worksheet.Cell("I51").FormulaA1 = "=SUM(I1:I50)";
-                worksheet.Cell("J51").FormulaA1 = "=SUM(J1:J50)";
-                worksheet.Cell("K51").FormulaA1 = "=SUM(A51:J51)";
-
-
-                workbook.SaveAs(enderecoSave + "Relatorio" + dataRelatorio + ".xlsx");
-                MessageBox.Show("Relatório Gerado em " + enderecoSave + " .");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro na geração do relatório");
             }
 
             //Process.Start(new ProcessStartInfo(@"C:\Users\ander\Downloads\Relatorio" + dataRelatorio + ".xlsx") { UseShellExecute = true });
@@ -303,9 +351,9 @@ namespace QRCodeACMnfp
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Não foi possível visitar a página. :(  \n=> Erro: ", ex.Message);
+                MessageBox.Show(ex.Message, "Não foi possível visitar a página.");
             }
-            txtQRCode.Focus();
+            FocoPrincipal();
         }
 
         private void VisitLink()
@@ -316,67 +364,104 @@ namespace QRCodeACMnfp
 
         private void button1_Click_1(object sender, EventArgs e) //fecha a aplicação
         {
-            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-            var result = MessageBox.Show("Já verificou se o relatorio foi emitido?", "LEMBRETE!", buttons, MessageBoxIcon.Question);
-            if (result == System.Windows.Forms.DialogResult.Yes)
+            try
             {
-                driver.Dispose();
-                Application.Exit();
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                var result = MessageBox.Show("Já verificou se o relatorio foi emitido?", "LEMBRETE!", buttons, MessageBoxIcon.Question);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    driver.Dispose();
+                    Application.Exit();
+                }
+                FocoPrincipal();
             }
-            txtQRCode.Focus();
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show("Erro ao fechar o aplicativo! => ", ex.Message);
+            }
+
         }
 
         private void btnExcluirRegistro_Click(object sender, EventArgs e)
         {
-            if (Notas.Count > 0)
+            try
             {
-                var nota = Notas.Last();
-                lancamentos--;
-                nota.Valor = 0.00f.ToString();
-                MostrarNoGridView();
-                Notas.Remove(nota);
-                AtualizarValoresTotaisNaTela();
-                MostrarNoGridView();
-                MessageBox.Show("Último registro foi excluído!");
+                if (Notas.Count > 0)
+                {
+                    var nota = Notas.Last();
+                    lancamentos--;
+                    nota.Valor = 0.00f.ToString();
+                    MostrarNoGridView();
+                    Notas.Remove(nota);
+                    AtualizarValoresTotaisNaTela();
+                    MostrarNoGridView();
+                    MessageBox.Show("Último registro foi excluído!");
+                }
+                else
+                {
+                    MessageBox.Show("Não há registros a serem excluídos", "Observação:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Não há registros a serem excluídos", "Observação:", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                MessageBox.Show("Erro ao tentar excluir um registro dos arquivos. ", ex.Message);
             }
+
+
             LimpaEFocaQRCode();
-        } //exclui 1 registro
+        } //exclui 1 registro //trycatch
 
         private void button3_Click(object sender, EventArgs e) //exclui todos registros
         {
-
-            LimparTodosDados();
-            MessageBox.Show("Todos registros foram limpos.!");
+            try
+            {
+                LimparTodosDados();
+                MessageBox.Show("Todos registros foram limpos.!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao limpar os registros. ", ex.Message);
+            }
         }
 
         private void LimparTodosDados()
         {
-            LimpaEFocaQRCode();
-            lancamentos = 0;
-            foreach(var nota in Notas)
+            try
             {
-                nota.Valor = 0.00f.ToString(); ;
+                LimpaEFocaQRCode();
+                lancamentos = 0;
+                foreach (var nota in Notas)
+                {
+                    nota.Valor = 0.00f.ToString(); ;
+                }
+                MostrarNoGridView();
+                Notas.Clear();
+                AtualizarValoresTotaisNaTela();
             }
-            MostrarNoGridView();
-            Notas.Clear();
-            AtualizarValoresTotaisNaTela();
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao Limpar todos os dados. ", ex.Message);
+            }
+
         }
 
         private void rdbAutomatico_CheckedChanged(object sender, EventArgs e)
         {
-            txtQRCode.Focus();
+            FocoPrincipal();
         }
 
         private void rdbManual_CheckedChanged(object sender, EventArgs e)
         {
-            txtQRCode.Focus();
+            FocoPrincipal();
         }
 
         private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            FocoPrincipal();
+        }
+
+        private void FocoPrincipal()
         {
             txtQRCode.Focus();
         }
--- QRCodeACMnfp/SeleniumSetMethods.cs
+++ QRCodeACMnfp/SeleniumSetMethods.cs
@@ -11,51 +11,70 @@ namespace QRCodeACMnfp
     {
         public static void EnterText(IWebDriver driver, string cnpj, string data, string valor, string extrato)
         {
-            
-            IWebElement webElementCnpf = driver.FindElement(By.Id("divCNPJEstabelecimento"));
-            IWebElement campoCnpj = webElementCnpf.FindElement(By.TagName("input"));
-            
-            IWebElement webElementDaTa = driver.FindElement(By.Id("divtxtDtNota"));
-            IWebElement campodata = webElementDaTa.FindElement(By.TagName("input"));
+            try
+            {
+                IWebElement webElementCnpf = driver.FindElement(By.Id("divCNPJEstabelecimento"));
+                IWebElement campoCnpj = webElementCnpf.FindElement(By.TagName("input"));
+
+                IWebElement webElementDaTa = driver.FindElement(By.Id("divtxtDtNota"));
+                IWebElement campodata = webElementDaTa.FindElement(By.TagName("input"));
 
 
-            IWebElement webElementExtrato = driver.FindElement(By.Id("divtxtNrNota"));
-            IWebElement campoExtrato = webElementExtrato.FindElement(By.TagName("input"));
+                IWebElement webElementExtrato = driver.FindElement(By.Id("divtxtNrNota"));
+                IWebElement campoExtrato = webElementExtrato.FindElement(By.TagName("input"));
 
-            IWebElement webElementValor = driver.FindElement(By.Id("divtxtVlNota"));
-            IWebElement campoValor = webElementValor.FindElement(By.TagName("input"));
-
-
-
-            campoCnpj.Clear();
-            campodata.Clear();
-            campoExtrato.Clear();
-            campoValor.Clear();
+                IWebElement webElementValor = driver.FindElement(By.Id("divtxtVlNota"));
+                IWebElement campoValor = webElementValor.FindElement(By.TagName("input"));
 
 
-            campoCnpj.SendKeys(cnpj);
-            campodata.SendKeys(data);
-            campoExtrato.SendKeys(extrato);
-            campoValor.SendKeys(valor);
+
+                campoCnpj.Clear();
+                campodata.Clear();
+                campoExtrato.Clear();
+                campoValor.Clear();
 
 
+                campoCnpj.SendKeys(cnpj);
+                campodata.SendKeys(data);
+                campoExtrato.SendKeys(extrato);
+                campoValor.SendKeys(valor);
+
+
+            }
+            catch (WebDriverArgumentException)
+            {
+                throw new WebDriverArgumentException("Erro no mapeamento e inserção dos elementos da página Web " +
+                    "no sistema da Nota fiscal paulista"); ;
+            }
         }
 
         public static void Click(IWebDriver driver, string element, string value, string elementtype)
         {
-            if (elementtype == "input")
-                driver.FindElement(By.Id(element)).Click();
-            //if (elementtype == "Name")
-            //    driver.FindElement(By.Name(elementtype)).Click();
+            try
+            {
+                if (elementtype == "input")
+                    driver.FindElement(By.Id(element)).Click();
+            }
+            catch (Exception)
+            {
+                throw new WebDriverArgumentException("Não foi possível encontra o CTA da página.");
+            }
         }
 
         public static void SelectDropDown(IWebDriver driver, string element, string value, string elementtype)
         {
-           
-            if (elementtype == "Id")
-           new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
-            if (elementtype == "Name")
-                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            try
+            {
+                if (elementtype == "Id")
+                    new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+                if (elementtype == "Name")
+                    new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            }
+            catch (WebDriverArgumentException)
+            {
+                throw new WebDriverArgumentException("Não foi possível encontrar o campo que define o tipo de cupom" +
+                    "fiscal na página.");
+            }
         }
     }
 }
